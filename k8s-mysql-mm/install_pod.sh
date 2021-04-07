034657d4575066b462b462ce1de50f967606d2a5
chelsea-nie <nieyunshuang@126.com>
2021-04-07 13:31:56 +0800

    Update install_pod.sh
    this script is the main script, just exec it.

--- k8s-mysql-mm/install_pod.sh
+++ k8s-mysql-mm/install_pod.sh
@@ -1,3 +1,13 @@
+####add label#####
+NODE_LIST=$(kubectl get nodes | awk '{if(NR != 1 )print $1}')
+for i in $NODE_LIST;do
+    kubectl label node $i app="mysql" --overwrite
+done
+
+#####create namespace ######
+kubectl create namespace data
+
+#######apply pod########
 kubectl apply -f mysql-configmap.yaml
 kubectl apply -f mysql-services.yaml
 kubectl apply -f mysql-statefulset.yaml
